1a1fc3260704c3bf5ff1ff3dc2121b54ab361713
SpGerg <spgerghelp@gmail.com>
2025-03-03 17:41:27 +0500

    chore: сейчас .gitignore

--- Dx.Core/API/Extensions/StringExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace Dx.Core.API.Extensions;
-
-public static class StringExtensions
-{
-    public static bool IsEveryone(this string content)
-    {
-        return content is "*" or "all";
-    }
-}
--- Dx.Core/API/Features/Webhooks/Enums/WebhookEventType.cs
+++
@@ -1,8 +0,0 @@
-namespace Dx.Core.API.Features.Webhooks.Enums;
-
-public enum WebhookEventType
-{
-    Error,
-    Warning,
-    Info
-}
